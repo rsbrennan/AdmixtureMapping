d9ca23384241d60e4af6b8ffc7377f8368658a6a
Reid Brennan <rsbrenna@farm.cse.ucdavis.edu>
2016-09-01 10:38:19 -0700

    annotation added

--- rename.sh
+++ rename.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -l
 
+
+#This renames column one in the second file based on the match from the first file, convert.scaff.name.txt. 
+#Also changes spaces to tabs (to make vcf normal compatible w/ most programs) via tr command
+#outputs vcf.gz and indexes the vcf with tabix.
+
 awk 'FNR==NR { a[$1]=$2; next } $1 in a { $1=a[$1] }1' \
 ~/reference/convert.scaff.name.txt ~/admixture_mapping/variants/cb-all.70.vcf |\
 tr ' ' '\t' |\
