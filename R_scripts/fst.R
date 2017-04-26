16e7ea28a8c6f68cb83b890bb0f3e11ea2c00eda
Reid Brennan <reid.brennan@gmail.com>
2017-04-26 16:58:57 -0700

    R scripts update

--- R_scripts/fst.R
+++ R_scripts/fst.R
@@ -2,7 +2,7 @@ library(qqman)
 library(corrplot)
 
 setwd("~/admixture_mapping/analysis/fst")
-filelist = list.files(pattern = ".*weir.fst")
+filelist = list.files(pattern = "*_vs_[A-Z][A-Z].weir.fst")
 
 #assuming tab separated values with a header     
 data_list = lapply(filelist, read.table, header=TRUE)
@@ -83,133 +83,6 @@ corrplot(values.2, method="color", type="upper",cl.lim=c(0,max(values)),
 	tl.col="black", cl.pos ="n",
 	diag=FALSE, tl.cex = 1.5,tl.srt=45)
 dev.off()
-
-
-#############################################################################
-#############################################################################
-############################ fst outliers ###################################
-#############################################################################
-#############################################################################
-
-library(mscr)
-
-################ pp vs pl ########
-#find 99% cutoff
-pp.pl <- data_list$PP_vs_PL.weir.fst
-pp.pl$SNP <- paste(pp.pl$CHR, pp.pl$BP, sep=":")
-pp.pl$CHR <- as.numeric((gsub("chr", "",pp.pl$CHR)))
-quant <- quantile(pp.pl$fst.1,c(0.95, 0.99), na.rm=TRUE)
-
-png("pp-pl_fst.png", h=1000, w=1700, pointsize=20)
-manhattan(pp.pl, suggestiveline = F, genomewideline = F, cex=0.7, 
-	main="Fst- PP vs. PL", p="fst.1", logp=FALSE, ylab="Fst", ylim=c(0,1.1), highlight=SNP)
-abline(h=quant[2], col='red')
-dev.off()
-
-outlie <- which(pp.pl$fst.1 > quant[2])
-pp.pl$outliers <- c("F")
-pp.pl$outliers[outlie] <- c("T")
-outs<-subset(pp.pl,pp.pl$outliers=="T")
-
-write.table(file="pp-pl.outliers.bed",cbind(outs$CHR,(outs$BP-1),outs$BP),sep="\t",quote=F,row.names=F,col.names=F)
-
-#merge adjoining outlier windows
-merged <- read.table("pp-pl.outliers.bed", header=F)
-#qu is a cutoff, if I want to remove those below a value, buff is how far to look when joining windows
-merged.win <-mergewin(win=merged, stat = outs$fst, qu= quant[2], tails="greater", buff=25000)
-mean(merged.win$end - merged.win$start)
-min(merged.win$end - merged.win$start)
-max(merged.win$end - merged.win$start)
-merged.win[(which((merged.win$end - merged.win$start) == max(merged.win$end - merged.win$start))),]
-
-write.table(merged.win,"pp-pl.merged.win",sep="\t",quote=F,row.names=F,col.names=F)
-
-
-#################### pc vs hp ########  
-pc.hp <- data_list$PC_vs_HP.weir.fst
-pc.hp$SNP <- paste(pc.hp$CHR, pc.hp$BP, sep=":")
-pc.hp$CHR <- as.numeric((gsub("chr", "",pc.hp$CHR)))
-#find 99% cutoff
-quant <- quantile(pc.hp$fst.1,c(0.95, 0.99), na.rm=TRUE)
-
-png("pc-hp_fst.png", h=1000, w=1700, pointsize=20)
-manhattan(pc.hp, suggestiveline = F, genomewideline = F, cex=0.7, 
-	main="Fst-PC vs. HP", p="fst.1", logp=FALSE, ylab="Fst", ylim=c(0,1.1), highlight = snp)
-abline(h=quant[2], col='red')
-dev.off()
-
-# identify outliers 
-outlie <- which(pc.hp$fst.1 > quant[2])
-pc.hp$outliers <- c("F")
-pc.hp$outliers[outlie] <- c("T")
-outs<-subset(pc.hp,pc.hp$outliers=="T")
-
-write.table(file="pc-hp.outliers.bed",cbind(outs$CHR,(outs$BP-1),outs$BP),sep="\t",quote=F,row.names=F,col.names=F)
-
-#merge adjoining outlier windows
-library(mscr)
-merged <- read.table("pc-hp.outliers.bed", header=F)
-#qu is a cutoff, if I want to remove those below a value, buff is how far to look when joining windows
-merged.win <-mergewin(win=merged, stat = outs$fst, qu= quant[2], tails="greater", buff=25000)
-mean(merged.win$end - merged.win$start)
-min(merged.win$end - merged.win$start)
-max(merged.win$end - merged.win$start)
-merged.win[(which((merged.win$end - merged.win$start) == max(merged.win$end - merged.win$start))),]
-
-write.table(merged.win,"pc-hp.merged.win",sep="\t",quote=F,row.names=F,col.names=F)
-
-################# pp vs pc ########
-
-pp.pc <- data_list$PP_vs_PC.weir.fst
-pp.pc$SNP <- paste(pp.pc$CHR, pp.pc$BP, sep=":")
-pp.pc$CHR <- as.numeric((gsub("chr", "",pp.pc$CHR)))
-#find 99% cutoff
-quant <- quantile(pp.pc$fst.1,c(0.95, 0.99), na.rm=TRUE)
-
-png("pp.pc_fst.png", h=1000, w=1700, pointsize=20)
-manhattan(pp.pc, suggestiveline = F, genomewideline = F, cex=0.7, 
-	main="Fst-PP vs. PC", p="fst.1", logp=FALSE, ylab="Fst", ylim=c(0,1.1), highlight = SNP)
-abline(h=quant[2], col='red')
-dev.off()
-
-# identify outliers 
-outlie <- which(pp.pc$fst.1 > quant[2])
-pp.pc$outliers <- c("F")
-pp.pc$outliers[outlie] <- c("T")
-outs<-subset(pp.pc,pp.pc$outliers=="T")
-
-write.table(file="pp-pc.outliers.bed",cbind(outs$CHR,(outs$BP-1),outs$BP),sep="\t",quote=F,row.names=F,col.names=F)
-
-#merge adjoining outlier windows
-library(mscr)
-merged <- read.table("pp-pc.outliers.bed", header=F)
-#qu is a cutoff, if I want to remove those below a value, buff is how far to look when joining windows
-merged.win <-mergewin(win=merged, stat = outs$fst, qu= quant[2], tails="greater", buff=25000)
-mean(merged.win$end - merged.win$start)
-min(merged.win$end - merged.win$start)
-max(merged.win$end - merged.win$start)
-merged.win[(which((merged.win$end - merged.win$start) == max(merged.win$end - merged.win$start))),]
-
-write.table(merged.win,"pp-pc.merged.win",sep="\t",quote=F,row.names=F,col.names=F)
-
-
-#############################################################################
-#############################################################################
-######################### overlap between bayescan and emp.##################
-#############################################################################
-#############################################################################
-pp.pl <- data_list$PP_vs_PL.weir.fst
-pp.pl$SNP <- paste(pp.pl$CHR, pp.pl$BP, sep=":")
-pp.pl$CHR <- as.numeric((gsub("chr", "",pp.pl$CHR)))
-quant <- quantile(pp.pl$fst.1,c(0.95, 0.99), na.rm=TRUE)
-pp.pl.bs <-read.table("~/admixture_mapping/analysis/bayescan/pp-pl.bayescan.bed")
-tohighlight <- paste(pp.pl.bs$V1, pp.pl.bs$V3, sep=":")
-
-manhattan(pp.pl, suggestiveline = F, genomewideline = F, cex=0.7, 
-	main="Fst- PP vs. PL", p="fst.1", logp=FALSE, ylab="Fst", ylim=c(0,1.1), highlight=tohighlight)
-abline(h=quant[2], col='red')
-
-
 
 
 
--- R_scripts/phenotypes.R
+++ R_scripts/phenotypes.R
@@ -23,7 +23,7 @@ df <- subset(dat.remove, pop=="AF" | pop=="BC")
 BC <- which(df$pop== "BC")
 AF <- which(df$pop== "AF")
 
-##### Temp ############
+##### ctmax ############
 
 temp <- data.frame(id=df$label, ctmax=df$ctmax)
 temp$pop <- substr(temp$id, 1,2)
@@ -40,18 +40,271 @@ points(x=df$mass[AF], y=df$ctmax[AF],
 a<- ggplot(temp, aes(pop, ctmax))
 a + geom_boxplot() + theme_classic()
 
-write.table(temp,"~/Documents/admixture_mapping/phenotypes/temp.id", 
+
+write.table(temp,"~/Documents/admixture_mapping/phenotypes/temp.af.bc.id", 
 	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
-write.table(temp[AF,],"~/Documents/admixture_mapping/phenotypes/temp.AF.id", 
+write.table(temp[AF,],"~/Documents/admixture_mapping/phenotypes/temp.af.id", 
 	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
-write.table(temp[BC,],"~/Documents/admixture_mapping/phenotypes/temp.BC.id", 
+write.table(temp[BC,],"~/Documents/admixture_mapping/phenotypes/temp.bc.id", 
 	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
-write.table(temp$ctmax,"~/Documents/admixture_mapping/phenotypes/temp.pheno", 
+write.table(temp$ctmax,"~/Documents/admixture_mapping/phenotypes/temp.af.bc.pheno", 
 	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
-write.table(temp$ctmax[AF],"~/Documents/admixture_mapping/phenotypes/temp.AF.pheno", 
+write.table(temp$ctmax[AF],"~/Documents/admixture_mapping/phenotypes/temp.af.pheno", 
 	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
-write.table(temp$ctmax[BC],"~/Documents/admixture_mapping/phenotypes/temp.BC.pheno", 
+write.table(temp$ctmax[BC],"~/Documents/admixture_mapping/phenotypes/temp.bc.pheno", 
 	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
+
+#fill in missing
+temp$ctmax[is.na(temp$ctmax)] <- c(33)
+write.table(temp$ctmax,"~/Documents/admixture_mapping/phenotypes/temp.af.bc.pheno.alter", 
+	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
+write.table(temp$ctmax[AF],"~/Documents/admixture_mapping/phenotypes/temp.af.pheno.alter", 
+	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
+write.table(temp$ctmax[BC],"~/Documents/admixture_mapping/phenotypes/temp.bc.pheno.alter", 
+	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
+
+#### include parent pops
+
+parent <- data.frame(id=dat.remove$label, ctmax=dat.remove$ctmax)
+parent$pop <- substr(parent$id, 1,2)
+
+df <- subset(parent, pop=="AF" | pop=="BC" | pop=="PP" | pop=="PL" | pop=="HP" | pop=="PC")
+
+write.table(df$ctmax,"~/Documents/admixture_mapping/phenotypes/temp.N_S.remove.pheno", 
+	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
+df <- subset(parent, pop=="AF" | pop=="PP" | pop=="PL")
+write.table(df$ctmax,"~/Documents/admixture_mapping/phenotypes/temp.cb.pheno", 
+	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
+df$ctmax[is.na(df$ctmax)] <- c(33)
+write.table(df$ctmax,"~/Documents/admixture_mapping/phenotypes/temp.N_S.remove.pheno.alter", 
+	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
+write.table(df$ctmax,"~/Documents/admixture_mapping/phenotypes/temp.cb.pheno.alter", 
+	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
+df <- subset(parent, pop=="BC" | pop=="HP" | pop=="PC")
+write.table(df$ctmax,"~/Documents/admixture_mapping/phenotypes/temp.ac.pheno", 
+	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
+write.table(df$ctmax,"~/Documents/admixture_mapping/phenotypes/temp.ac.pheno.alter", 
+	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
+
+
+
+#########
+#########
+#########
+######### Hypoxia #############
+#########
+#########
+#########
+
+#Hypoxia: remove high mass outliers and/or mass as covariate
+#probably mass as a covariate. I don't really see many outliers that can be removed.
+
+dat <- read.csv("~/Documents/admixture_mapping/Coastal\ Project\ Data\ Summary_MASTER.csv")
+ordered <- dat[order(dat$label),]
+#remove indivs due to low coverege and the weird PC indivs
+dat.remove <- subset(ordered, label!="AF-634-141" & label!="AF-635-142" & 
+	label!="AF-636-143" & label!="AF-637-144" & label!="AF-638-145" &
+	label!="AF-639-146" & label!="AF-640-147" & label!="AF-641-148" &
+	label!="AF-506-244" & label!="BC-132-088" & label!="PC-369-001" & 
+	label!="PC-386-017" )
+
+
+parent <- data.frame(id=dat.remove$label, hploe=dat.remove$hploe, mass=dat.remove$hpmass)
+parent$pop <- substr(parent$id, 1,2)
+
+df <- subset(parent, pop=="AF" | pop=="BC" | pop=="PP" | pop=="PL" | pop=="HP" | pop=="PC")
+#df <- subset(dat.remove, pop=="AF" | pop=="BC")
+BC <- which(df$pop== "BC")
+AF <- which(df$pop== "AF")
+
+summary(lm(df$hploe ~ df$mass))
+
+a <- ggplot(df, aes(x=mass, y=hploe))+
+geom_point(aes(col=df$pop))+ theme_classic()+
+ geom_smooth(method=lm)
+a
+
+hypoxia <- data.frame(id=df$id, hploe=df$hploe, mass=df$mass)
+hypoxia$pop <- substr(hypoxia$id, 1,2)
+
+
+a<- ggplot(hypoxia, aes(pop, hploe))
+a + geom_boxplot() + theme_classic()
+
+write.table(subset(hypoxia, pop=="AF"| pop=="BC")$hploe,
+	"~/Documents/admixture_mapping/phenotypes/hploe.af.bc.pheno", 
+	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
+write.table(subset(hypoxia, pop=="AF")$hploe,
+	"~/Documents/admixture_mapping/phenotypes/hploe.af.pheno", 
+	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
+write.table(subset(hypoxia, pop=="BC")$hploe,
+	"~/Documents/admixture_mapping/phenotypes/hploe.bc.pheno", 
+	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
+
+## include parent pops
+write.table(hypoxia$hploe,
+	"~/Documents/admixture_mapping/phenotypes/hploe.N_S.remove.pheno", 
+	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
+write.table(subset(hypoxia, pop=="BC"| pop=="HP" | pop=="PC")$hploe,
+	"~/Documents/admixture_mapping/phenotypes/hploe.ac.pheno", 
+	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
+write.table(subset(hypoxia, pop=="AF"| pop=="PP" | pop=="PL")$hploe,
+	"~/Documents/admixture_mapping/phenotypes/hploe.cb.pheno", 
+	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
+
+
+### Write hpmass covariate
+
+write.table(subset(hypoxia, pop=="AF"| pop=="BC")$mass,
+	"~/Documents/admixture_mapping/phenotypes/hploe.af.bc.mass", 
+	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
+write.table(subset(hypoxia, pop=="AF")$mass,
+	"~/Documents/admixture_mapping/phenotypes/hploe.af.mass", 
+	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
+write.table(subset(hypoxia, pop=="BC")$mass,
+	"~/Documents/admixture_mapping/phenotypes/hploe.bc.mass", 
+	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
+
+## include parent pops
+write.table(hypoxia$mass,
+	"~/Documents/admixture_mapping/phenotypes/hploe.N_S.remove.mass", 
+	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
+write.table(subset(hypoxia, pop=="BC"| pop=="HP" | pop=="PC")$mass,
+	"~/Documents/admixture_mapping/phenotypes/hploe.ac.mass", 
+	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
+write.table(subset(hypoxia, pop=="AF"| pop=="PP" | pop=="PL")$mass,
+	"~/Documents/admixture_mapping/phenotypes/hploe.cb.mass", 
+	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
+
+
+
+
+
+
+#########
+#########
+#########
+######### pchl #############
+#########
+#########
+#########
+
+#chloride: theres an effect of mass. removing outliers, 
+#then trying to take out mass effect. 
+#I'm removing outliers, tim is taking out effect of mass
+dat <- read.csv("~/Documents/admixture_mapping/Coastal\ Project\ Data\ Summary_MASTER.csv")
+ordered <- dat[order(dat$label),]
+#remove indivs due to low coverege and the weird PC indivs
+dat.remove <- subset(ordered, label!="AF-634-141" & label!="AF-635-142" & 
+	label!="AF-636-143" & label!="AF-637-144" & label!="AF-638-145" &
+	label!="AF-639-146" & label!="AF-640-147" & label!="AF-641-148" &
+	label!="AF-506-244" & label!="BC-132-088" & label!="PC-369-001" & 
+	label!="PC-386-017" )
+
+parent <- data.frame(id=dat.remove$label, pchl=dat.remove$pchl, mass=dat.remove$spmass)
+parent$pop <- substr(parent$id, 1,2)
+
+df <- subset(parent, pop=="AF" | pop=="BC" | pop=="PP" | pop=="PL" | pop=="HP" | pop=="PC")
+
+
+#salinity <- subset(df, pop=="AF" | pop=="BC")
+new <- df[!is.na(df$pchl),]
+new.na <- df[is.na(df$pchl),]
+
+library(outliers)
+
+a <-scores(new$pchl, type="chisq", prob=0.95)
+b <-scores(new$pchl, type="chisq", prob=0.99)
+
+write.table(new[!a,], "~/Documents/admixture_mapping/phenotypes/pchl.95.txt",
+		row.names=FALSE, col.names=TRUE, quote=FALSE, sep="\t"
+)
+
+write.table(new[!b,], "~/Documents/admixture_mapping/phenotypes/pchl.99.txt",
+		row.names=FALSE, col.names=TRUE, quote=FALSE, sep="\t"
+)
+
+
+#change outliers to NA
+a <-scores(new$pchl, type="chisq", prob=0.95)
+new$pchl.mod <- new$pchl
+new$pchl.mod[a] <- c(NA)
+new.na$pchl.mod <- c(NA)
+combined <- rbind(new, new.na)
+combined.ordered <- combined[order(combined$id),]
+
+#write new dataframe
+chloride <- data.frame(id=combined.ordered$id, pchl=combined.ordered$pchl.mod, mass=combined.ordered$mass)
+chloride$pop <- substr(chloride$id, 1,2)
+
+#write phenotype to file
+write.table(subset(chloride, pop=="AF"| pop=="BC")$pchl,
+	"~/Documents/admixture_mapping/phenotypes/pchl.af.bc.pheno", 
+	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
+write.table(subset(chloride, pop=="AF")$pchl,
+	"~/Documents/admixture_mapping/phenotypes/pchl.af.pheno", 
+	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
+write.table(subset(chloride, pop=="BC")$pchl,
+	"~/Documents/admixture_mapping/phenotypes/pchl.bc.pheno", 
+	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
+
+# include parent pops
+write.table(chloride$pchl,
+	"~/Documents/admixture_mapping/phenotypes/pchl.N_S.remove.pheno", 
+	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
+write.table(subset(chloride, pop=="BC"| pop=="HP" | pop=="PC")$pchl,
+	"~/Documents/admixture_mapping/phenotypes/pchl.ac.pheno", 
+	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
+write.table(subset(chloride, pop=="AF"| pop=="PP" | pop=="PL")$pchl,
+	"~/Documents/admixture_mapping/phenotypes/pchl.cb.pheno", 
+	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
+
+
+### Write hpmass covariate
+
+write.table(subset(chloride, pop=="AF"| pop=="BC")$mass,
+	"~/Documents/admixture_mapping/phenotypes/pchl.af.bc.mass", 
+	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
+write.table(subset(chloride, pop=="AF")$mass,
+	"~/Documents/admixture_mapping/phenotypes/pchl.af.mass", 
+	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
+write.table(subset(chloride, pop=="BC")$mass,
+	"~/Documents/admixture_mapping/phenotypes/pchl.bc.mass", 
+	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
+
+## include parent pops
+write.table(chloride$mass,
+	"~/Documents/admixture_mapping/phenotypes/pchl.N_S.remove.mass", 
+	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
+write.table(subset(chloride, pop=="BC"| pop=="HP" | pop=="PC")$mass,
+	"~/Documents/admixture_mapping/phenotypes/pchl.ac.mass", 
+	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
+write.table(subset(chloride, pop=="AF"| pop=="PP" | pop=="PL")$mass,
+	"~/Documents/admixture_mapping/phenotypes/pchl.cb.mass", 
+	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
+
+
+
+
+
+
+
+
+######### Metabolic Rate
+#MO2: effect of mass and year. Include mass as covariate. 
+# could also try to mass Mass correct... including a covariate seems easy
+# Tim sent mass corrected values for the admixed populations alone. 
+# the confounding issue of year is a problem... might not be able to combine these. 
+## could combine p values from two sep analyses??
+
+
+######### Morphology
+#morphology: size correct http://www.flywings.org.uk/MorphoJ_guide/frameset.htm?covariation/regression.htm
+
+
+
+
+
 
 ###### Hematocrit ######
 
@@ -76,88 +329,4 @@ r2 <- resid(lm(dat.remove$hct ~ dat.remove$sex, na.action=na.exclude))
 ggplot(dat.remove, aes(x = dat.remove$pop, y = r2)) + 
     geom_boxplot()
 
-
-###### Salinity ########
-
-salinity <- subset(df, pop=="AF" | pop=="BC")
-new <- salinity[!is.na(salinity$pchl),]
-
-
-#The (1.5 * IQR) criteria for finding outliers (1.5 times IQR):
-#An observation is suspected outliers if it is:
-#Below Q1 - (1.5 * IQR): that is Q1 minus 1.5 times IQR.
-#Above Q3 + (1.5 * IQR): that is Q2 minus 1.5 times IQR.
-
-quantile(new$pchl)
-lower <- which(new$pchl < quantile(new$pchl, 0.25) - (IQR(new$pchl)*1.5))
-upper <- which(new$pchl > quantile(new$pchl, 0.75) + (IQR(new$pchl)*1.5))
-all.outlier <- c(lower, upper)
-
-library(outliers)
-
-a <-scores(new$pchl, type="chisq", prob=0.95)
-b <-scores(new$pchl, type="chisq", prob=0.99)
-
-write.table(new[!a,], "~/Documents/admixture_mapping/phenotypes/pchl.95.txt",
-		row.names=FALSE, col.names=TRUE, quote=FALSE, sep="\t"
-)
-
-write.table(new[!b,], "~/Documents/admixture_mapping/phenotypes/pchl.99.txt",
-		row.names=FALSE, col.names=TRUE, quote=FALSE, sep="\t"
-)
-
-
-plot(new$spmass[a],new$pchl[a], ylim=c(50, 165), xlim=c(1.5, 16))
-plot(new$spmass[b],new$pchl[b],ylim=c(50, 165), xlim=c(1.5, 16))
-plot(new$spmass,new$pchl, ylim=c(50, 165), xlim=c(1.5, 16))
-
-b <-scores(new$pchl, type="chisq", prob=0.99)
-plot(new$spmass[b],new$pchl[b],ylim=c(50, 165), xlim=c(1.5, 16))
-
-
-
-BC <- which(salinity$pop== "BC")
-AF <- which(salinity$pop== "AF")
-
-
-
-
-
-a<- ggplot(salinity, aes(log(spmass), pchl))
-a + geom_point(aes(color=pop)) + theme_classic()+
-scale_color_manual(values=c("tomato3", "royalblue"))
-
-summary((lm(salinity$pchl ~ log(salinity$spmass), na.action=na.exclude)))
-
-options(contrasts = c("contr.treatment", "contr.poly"))
-r1 <- resid(lm(salinity$pchl ~ salinity$spmass, na.action=na.exclude))
-
-a<- ggplot(salinity, aes(mass, r1))
-a + geom_point(aes(color=pop)) + theme_classic()+
-scale_color_manual(values=c("tomato3", "royalblue"))
-
-cor.test(salinity$spmass,r1) # -> r = 3.019388e-1
-
-summary((lm(r1 ~ salinity$spmass, na.action=na.exclude)))
-summary((lm(r1 ~ salinity$pop, na.action=na.exclude)))
-
-mod <- lm(salinity$pchl ~ salinity$spmass)
-
-a<- ggplot(df, aes(pop, pchl))
-a + geom_boxplot() + theme_classic()
-
-#
-test <- data.frame(pop=df$pop[(!is.na(df$pchl) & !is.na(df$spmass))], pchl=mod$fitted.values)
-
-a<- ggplot(test, aes(pop, pchl))
-a + geom_boxplot() + theme_classic()
-
-write.table(r1,"~/Documents/admixture_mapping/phenotypes/salinity.id", 
-	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
-
-write.table(cbind(df$label, r1),"~/Documents/admixture_mapping/phenotypes/salinity.pheno", 
-	row.names=FALSE, col.names=FALSE, quote=FALSE, sep="\t")
-
-
-#########
 
